d4fe70271838f82ead515dbf1205755cf05d2c1e
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-11-24 00:18:25 -0800

    compressed path quick union

---
+++ compressed_path_quick_union.rb
@@ -0,0 +1,32 @@
+class QuickUnion
+  attr_accessor :ids, :sizes
+
+  def initialize(number_of_nodes)
+    @ids = (0..number_of_nodes).to_a
+    @sizes = Array.new(number_of_nodes, 1)
+  end
+
+  def root(node)
+    until node == ids[node]
+      ids[node] = ids[ids[node]]
+      node = ids[node]
+    end
+    node
+  end
+
+  def connected?(first_node, second_node)
+    root(first_node) == root(second_node)
+  end
+
+  def union(first_node, second_node)
+    first_root = root(first_node)
+    second_root = root(second_node)
+    if sizes[first_root] < sizes[second_root]
+      ids[first_root] = second_root
+      sizes[second_root] += sizes[first_root]
+    else
+      ids[second_root] = second_root
+      sizes[first_root] += sizes[second_root]
+    end
+  end
+end
